f2565883dd822477a1683a8d5c6d3de5e8752ed0
NothernWind <kaljan.nothern@gmail.com>
2015-07-23 18:42:37 -0400

    0.0.7

--- pwm_control.cpp
+++ pwm_control.cpp
@@ -267,19 +267,20 @@ void PWM_Control::send_pwm_request()
 	paket.period = tim_period;
 	paket.duty = pwm_duty;
 
-//	spi_ptcl->setPWM_Params(paket);
+	spi_ptcl->setPWM_Params(paket);
 }
 
 void PWM_Control::single_slicked()
 {
 	send_pwm_request();
-	spi_ptcl->setPWM_Params(&paket);
+//	spi_ptcl->setPWM_Params(&paket);
 }
 
 void PWM_Control::timer_update()
 {
 	if (start_state == true) {
 		send_pwm_request();
-		spi_ptcl->setPWM_Params(&paket);
+//		spi_ptcl->setPWM_Params(&paket);
+		timer->start(20);
 	}
 }
