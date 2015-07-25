8095891ab8324769af2ad03da1dcdb1f0d8d39b2
NothernWind <kaljan.nothern@gmail.com>
2015-07-24 22:45:12 -0400

    0.0.7

--- motorcontrol.cpp
+++ motorcontrol.cpp
@@ -9,6 +9,8 @@ MotorControl::MotorControl(QWidget *parent)
 void MotorControl::create_mc() {
 	grid = new QGridLayout(this);
 	FreqBox = new QSpinBox(this);
+	RunButton = new QPushButton("run", this);
+
 	PWM_CH1P = new QVExtSlider("CH1P", this);
 	PWM_CH2P = new QVExtSlider("CH2P", this);
 	PWM_CH3P = new QVExtSlider("CH3P", this);
@@ -26,11 +28,11 @@ void MotorControl::create_mc() {
 	grid->addWidget(RunButton, 1, 0, 1, 3);
 
 	grid->addWidget(PWM_CH1P, 2, 0);
-	grid->addWidget(PWM_CH2P, 2, 1);
+	grid->addWidget(PWM_CH2P, 2, 2);
 	grid->addWidget(PWM_CH3P, 2, 2);
 
-	grid->addWidget(PWM_CH1N, 2, 0);
-	grid->addWidget(PWM_CH2N, 2, 1);
-	grid->addWidget(PWM_CH3N, 2, 2);
+	grid->addWidget(PWM_CH1N, 3, 0);
+	grid->addWidget(PWM_CH2N, 3, 1);
+	grid->addWidget(PWM_CH3N, 3, 2);
 
 }
